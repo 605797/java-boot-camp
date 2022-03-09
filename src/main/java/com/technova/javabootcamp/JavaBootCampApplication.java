7e33bd5fb34e2eb2abde44e32ce72339b3ad6c3b
Siddhant Thapa Chhetry <90285183+605797@users.noreply.github.com>
2022-03-09 17:46:34 -0600

    Initial commit

---
+++ src/main/java/com/technova/javabootcamp/JavaBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.technova.javabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technova/javabootcamp/JavaBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technova.javabootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
